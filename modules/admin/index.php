04de0bba734c72a834de522a9e6329c2cd2f719b
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-10 01:45:01 +0300

    fixed dashboard

--- modules/admin/index.php
+++ modules/admin/index.php
@@ -1,21 +1,5 @@
 <?php
-include '../../connection/index.php';
-
-  $query="select * from user where role = '1'";
-  $result=mysqli_query($connect,$query);
-  $count=mysqli_fetch_assoc($result);
-  $total_admin = mysqli_num_rows($result);
-
-  $query="select * from user where role = '2'";
-  $result1=mysqli_query($connect,$query);
-  $count1=mysqli_fetch_assoc($result1);
-  $total_student = mysqli_num_rows($result1);
-
-  $query="select * from user";
-  $result4=mysqli_query($connect,$query);
-  // $count=mysqli_fetch_assoc($result4);
-  $total_users = mysqli_num_rows($result4);
-
+include '../../api/dashboard.php';
  ?>
 
 
