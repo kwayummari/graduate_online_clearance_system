02e8f5c53657ff4d405a2f026c873821c3561db6
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-10 01:14:53 +0300

    fixed login.php

--- api/login.php
+++ api/login.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
- include '../connection/index.php';
+ include 'connection/index.php';
 if (isset($_POST['submit'])) {
   $email=$_POST['email'];
-  $password=$_POST['password'];
+  $password=base64_encode($_POST['password']);
   $query="select * from user where email='$email' and password='$password'";
   $result=mysqli_query($connect,$query);
   $count=mysqli_fetch_assoc($result);
 
   if($count) {
-    $_SESSION['id'] = $count['id'];
+    $_SESSION['user_id'] = $count['id'];
     $_SESSION['role'] = $count['role'];
-    $_SESSION['name'] = $username;  
-  if ($count['role'] == 'admin') {
-      header("location: home/");
+  if ($count['role'] == '1') {
+    //   header("location: home/");
+    echo 'admin';
   }
 } else {
   echo 'Wrong email/ password';
