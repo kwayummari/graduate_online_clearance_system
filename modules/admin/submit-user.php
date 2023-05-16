5f89cb102ce5ffa3d9c51f06f8cffdfce44508f2
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-16 18:48:42 +0300

    fixed add ing user api

--- modules/admin/submit-user.php
+++ modules/admin/submit-user.php
@@ -4,9 +4,10 @@ if (isset($_POST['submit'])) {
   $fname=$_POST['fname'];
   $email=$_POST['email'];
   $role=$_POST['role'];
+  $id_number=$_POST['id_number'];
   $password=base64_encode($_POST['password']);
-  $query="INSERT INTO user (fullname,email,role,password,status)
-    values('$fname', '$email', '$role', '$password', '1')";
+  $query="INSERT INTO user (fullname,email,role,password,status,id_number)
+    values('$fname', '$email', '$role', '$password', '1', '$id_number')";
   if(mysqli_query($connect,$query)) {
     echo "Succefully added user";
 } else {
