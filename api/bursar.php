138a832fa72d3231a39406ae049c5e77d43553b2
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 19:59:06 +0300

    fixed bursar.php

--- api/bursar.php
+++ api/bursar.php
@@ -1,6 +1,6 @@
 <?php
 include '../../connection/index.php';
-  $query="select * from clearance where stage = '3' and status = '1' and dean = '0'";
+  $query="select * from clearance where stage = '3' and status = '1' and bursar = '0'";
   $result4=mysqli_query($connect,$query);
   $total_clearance = mysqli_num_rows($result4);
  ?>
