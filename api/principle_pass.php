0354884a18ca54f25697675011698e185821b88e
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 19:03:59 +0300

    fixed daruso index.php

--- api/principle_pass.php
+++ api/principle_pass.php
@@ -1,7 +1,7 @@
 <?php
 include '../connection/index.php';
 $id = base64_decode($_GET['id']);
-$query="update clearance set status = '0', percent = '2', warden = '0', usab = '0' where id = '$id'";
+$query="update clearance set status = '0', percent = '2', warden = '0', usab = '0', daruso = '0' where id = '$id'";
   $insert=mysqli_query($connect,$query);
   if (isset($_SERVER["HTTP_REFERER"])) {
     header("Location: " . $_SERVER["HTTP_REFERER"]);
