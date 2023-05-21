32b5b09cbfde17828834dfb9b3151b4bbfac7247
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 19:20:42 +0300

    fixed library api

--- api/library.php
+++ api/library.php
@@ -1,6 +1,6 @@
 <?php
 include '../../connection/index.php';
-  $query="select * from clearance where stage = '3' and status = '1' and usab = '0'";
+  $query="select * from clearance where stage = '3' and status = '1' and library = '0'";
   $result4=mysqli_query($connect,$query);
   $total_clearance = mysqli_num_rows($result4);
  ?>
