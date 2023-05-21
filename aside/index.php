20a56e2e716f135b95d328ab899a893c7a7bd5d7
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 18:03:54 +0300

    fixed aside

--- aside/index.php
+++ aside/index.php
@@ -7,8 +7,8 @@ if($role == '1'){
 else if($role == '3'){
   include 'convacation-aside.php';
 } else if($role == '4') {
-  echo 'convacation-aside.php';
+  include 'convacation-aside.php';
 } else if($role == '5') {
-  echo 'convacation-aside.php';
+  include 'convacation-aside.php';
 }
 ?>
