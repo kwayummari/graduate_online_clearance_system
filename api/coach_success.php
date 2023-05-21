837e416a5598ebd50aa0ef20a2f776f320c131a2
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 19:51:54 +0300

    creacted coach succes

---
+++ api/coach_success.php
@@ -0,0 +1,16 @@
+<?php
+include '../connection/index.php';
+$id = base64_decode($_GET['id']);
+$query = "SELECT percent FROM clearance WHERE id = '$id'";
+$result = mysqli_query($connect, $query);
+$row = mysqli_fetch_assoc($result);
+$currentValue = $row['percent'];
+
+// Add 1 to the current value
+$newValue = $currentValue + 1;
+$query1="update clearance set status = '0', percent = '$newValue', coach = '1' where id = '$id'";
+  $insert=mysqli_query($connect,$query1);
+  if (isset($_SERVER["HTTP_REFERER"])) {
+    header("Location: " . $_SERVER["HTTP_REFERER"]);
+}
+ ?>
