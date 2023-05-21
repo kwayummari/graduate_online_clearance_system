ea97b50c47d519d41da4befeba4efe77117c4fd9
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 20:21:30 +0300

    fixed principle pass

--- api/principle_pass.php
+++ api/principle_pass.php
@@ -1,7 +1,7 @@
 <?php
 include '../connection/index.php';
 $id = base64_decode($_GET['id']);
-$query="update clearance set status = '0', percent = '2', warden = '0', usab = '0', daruso = '0', library = '0', dean = '0', coach = '0', bursar = '0', card = '0' where id = '$id'";
+$query="update clearance set status = '0', percent = '2', warden = '0', usab = '0', daruso = '0', library = '0', dean = '0', coach = '0', bursar = '0', card = '0', loan = '0' where id = '$id'";
   $insert=mysqli_query($connect,$query);
   if (isset($_SERVER["HTTP_REFERER"])) {
     header("Location: " . $_SERVER["HTTP_REFERER"]);
