1b8bd2b14c243fe8bde819383cbfc35d594345ad
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 20:02:34 +0300

    fixed bursar success

--- api/bursar_success.php
+++ api/bursar_success.php
@@ -8,7 +8,7 @@ $currentValue = $row['percent'];
 
 // Add 1 to the current value
 $newValue = $currentValue + 1;
-$query1="update clearance set status = '0', percent = '$newValue', dean = '1' where id = '$id'";
+$query1="update clearance set status = '0', percent = '$newValue', bursar = '1' where id = '$id'";
   $insert=mysqli_query($connect,$query1);
   if (isset($_SERVER["HTTP_REFERER"])) {
     header("Location: " . $_SERVER["HTTP_REFERER"]);
