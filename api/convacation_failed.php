f3d14c247113f637ed4efcf920ebbc12206b92ca
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 14:08:47 +0300

    deleted delete.php in student folder

--- api/convacation_failed.php
+++ api/convacation_failed.php
@@ -1,10 +1,13 @@
 <?php
-include '../../connection/index.php';
+if (isset($_POST['submit'])) {
 $id = $_POST['id'];
 $remarks = $_POST['remarks'];
 $query="update clearance set status = '2', remarks = '$remarks' where id = '$id'";
   $insert=mysqli_query($connect,$query);
-  if (isset($_SERVER["HTTP_REFERER"])) {
-    header("Location: " . $_SERVER["HTTP_REFERER"]);
+  if($insert) {
+    echo "Remarks updated successfully";
+} else {
+    echo "Failed to update remarks";
+}
 }
  ?>
