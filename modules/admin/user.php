c653714a53a6d7ae29618290036de351ac7545ba
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 20:19:29 +0300

    fixed user.php

--- modules/admin/user.php
+++ modules/admin/user.php
@@ -65,6 +65,8 @@ include '../../api/dashboard.php';
                           echo 'Bursar';
                         } else if($rows4['role'] == '12') {
                           echo 'Smart Card';
+                        } else if($rows4['role'] == '13') {
+                          echo 'Loan Board';
                         }
                         ?></span></td>
                         <td><a href="update.php?id=<?php echo base64_encode($rows4['id']); ?>"><i class="bi bi-pen" style="color: green;"></i></a></td>
