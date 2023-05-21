1f3e0f931778376f8ed8f55ce605f95e78358b14
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-21 14:49:35 +0300

    fixed convacation success

--- api/convacation_success.php
+++ api/convacation_success.php
@@ -2,7 +2,6 @@
 include '../connection/index.php';
 $id = base64_decode($_GET['id']);
 $query="update clearance set status = '0' where id = '$id'";
-  $query="insert into clearance (student_id,stage,status,remarks) values('$user_id','1','1', '0')";
   $insert=mysqli_query($connect,$query);
   if (isset($_SERVER["HTTP_REFERER"])) {
     header("Location: " . $_SERVER["HTTP_REFERER"]);
