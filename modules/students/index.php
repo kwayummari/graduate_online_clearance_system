e6c6f163e63bb7815601c4452f40d255caf0bc24
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-22 14:02:03 +0300

    fixed index student

--- modules/students/index.php
+++ modules/students/index.php
@@ -77,7 +77,10 @@ include '../../api/students.php';
               <div class="col-xxl-12 col-md-6">
               <div class="card info-card sales-card">
                 <div class="card-body">
-                  <h5 class="card-title">Remarks <span>|The following  are comments of to why your clearance to be cancelled</span></h5>
+                  <h5 class="card-title">Remarks <span>|<?php if($rows4['percent'] != 12) { ?>
+                       The following  are comments of to why your clearance to be cancelled <?php } else { ?>
+                        Thank you for finishing your clearance process
+                        <?php } ?></span></h5>
                   <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                       <i class="bi bi-book"></i>
