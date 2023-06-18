b2ebe8cca60901f73388b6c3c8c7926df4b72ec6
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-18 20:50:14 +0300

    fixed user registration

--- modules/admin/submit-user.php
+++ modules/admin/submit-user.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])) {
   $id_number=$_POST['id_number'];
   $password=base64_encode($_POST['password']);
   $query="INSERT INTO user (fullname,email,role,password,status,id_number,program)
-    values('$fname', '$email', '$role', '$password', '1', '$id_number','$program)";
+    values('$fname', '$email', '$role', '$password', '1', '$id_number','$program')";
   if(mysqli_query($connect,$query)) {
     echo "Succefully added user";
 } else {
