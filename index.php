eb422fc5ab68dcde7bc720c37dc8123ac4850398
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-18 20:45:31 +0300

    fixed logo in head

--- index.php
+++ index.php
@@ -19,21 +19,14 @@
                   <span class="d-none d-lg-block align-text-center" style="color: white">GOCS PANEL</span>
                 </a>
               </div><!-- End Logo -->
-
               <div class="card mb-3">
-
                 <div class="card-body">
-
                   <div class="pt-4 pb-2">
                     <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                     <p class="text-center small">Enter your username & password to login</p>
                   </div>
                   <h5 class="card-title"><span style="color: red;"><b><?php include('api/login.php'); ?></b></span> </h5>
-
-              <!-- General Form Elements -->
                   <form method="POST" class="row g-3 needs-validation" target="_top" autocomplete="on"   enctype="multipart/form-data" novalidate>
-                  <!--<form method="POST" class="row g-3 needs-validation" novalidate>-->
-
                     <div class="col-12">
                       <label for="yourUsername" class="form-label">Identification Number</label>
                       <div class="input-group has-validation">
@@ -59,13 +52,10 @@
                       <button class="btn btn-primary w-100" style='background-color: #1e5288' name="submit" type="submit">Login</button>
                     </div>
                     <div class="col-12">
-                      <!--<p class="small mb-0">Don't have account? <a href="register.html">Create an account</a></p>-->
                     </div>
                   </form>
-
                 </div>
               </div>
-
               <div class="credits" style = "color: white">
                 Designed and Maintained by <a href="#" style = "color: red">Mariam Ahmad</a>
               </div>
@@ -78,10 +68,7 @@
 
     </div>
   </main><!-- End #main -->
-
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
   <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/chart.js/chart.min.js"></script>
@@ -90,8 +77,6 @@
   <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
   <script src="assets/vendor/tinymce/tinymce.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
 
 </body>
