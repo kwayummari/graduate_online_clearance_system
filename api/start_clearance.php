bdead6b4c226dd90f1066220528712772e08bd8e
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-27 10:07:19 +0300

    fixed date

--- api/start_clearance.php
+++ api/start_clearance.php
@@ -1,6 +1,7 @@
 <?php
 include '../connection/index.php';
-  $query="insert into clearance (student_id,stage,status,remarks) values('$user_id','1','1', '0')";
+$today = date('Y-m-d');
+  $query="insert into clearance (student_id,stage,status,remarks,date) values('$user_id','1','1', '0','$today')";
   $insert=mysqli_query($connect,$query);
   if (isset($_SERVER["HTTP_REFERER"])) {
     header("Location: " . $_SERVER["HTTP_REFERER"]);
